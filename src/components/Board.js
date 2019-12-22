37da48b8dc5d7bbcb6c0347c8bd407862c414ae9
Alvin Ng <ngalvin93@gmail.com>
2019-12-22 00:18:31 -0600

    wip square click

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,36 +3,17 @@ import Square from './Square';
 import { connect } from 'react-redux';
 import { clickSquare } from '../actions/index'
 
-
 class Board extends React.Component {
-    handleClick(i) {
-        console.log(this.props.state.squares, '****')
-        // const squares = this.props.state.squares.slice(); //Create copy of squares array instead of changing the existing array in order to not mutate underlying data set
-        // const squares = i.target
-        // squares[i] = ' ';
-        // this.setState({ squares: squares})
-        this.props.clickSquare()
-        
-    }
-
-    renderSquare(i) {
-        return (<Square 
-        color = {this.props.state.squares[i]}
-        value= {this.props.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
-        />
-        );
-    }
-
     render() {
         const { squares } = this.props.state.squares
         const mappedBoard = []
         for (let i = 0; i < 10; i++) {
             squares[i].map((singleSquare, idx) => {
                 console.log('single square', singleSquare + idx)
-                mappedBoard.push(<Square color={singleSquare.color} />)
+                mappedBoard.push(<Square color={singleSquare.color} onClick={this.props.clickSquare} />)
             })
         }
+
         return (
             <div id="boardDiv">
                 { mappedBoard }
@@ -40,7 +21,6 @@ class Board extends React.Component {
         )
     }
 }
-
 
 const mapStateToProps = state => {
     return { state }
