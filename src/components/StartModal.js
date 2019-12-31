af4aafd5bc26908febef65b2f4e7cea2412cbd29
Alvin Ng <ngalvin93@gmail.com>
2019-12-31 14:45:19 -0600

    copy share link to clipboard

--- src/components/StartModal.js
+++ src/components/StartModal.js
@@ -6,11 +6,24 @@ import { connect } from 'react-redux'
 function StartModal (props) {
   const url = window.location.href
   const [modal, setModal] = useState(false)
+
   const toggle = () => {
     setModal(!modal)
     props.firebaseAction(props.state.squares)
   }
   const backdrop = true
+
+  const handleCopyLink = () => {
+    console.log('copy link')
+    const copyBtn = document.getElementById('copyBtn')
+    var dummy = document.createElement('textarea')
+    document.body.appendChild(dummy)
+    dummy.value = window.location.href + 'game/' + props.props.gameId
+    dummy.select()
+    document.execCommand('copy')
+    document.body.removeChild(dummy)
+    copyBtn.innerHTML = 'Copied!'
+  }
 
   return (
     <div>
@@ -21,7 +34,7 @@ function StartModal (props) {
           Connect with your opponent by sharing this link: {url}game/{props.props.gameId}
         </ModalBody>
         <ModalFooter>
-          <Button color='primary' onClick={toggle}>Copy Link</Button>{' '}
+          <Button id='copyBtn' color='primary' onClick={ handleCopyLink }>Copy Link</Button>{' '}
           <Button color='secondary' onClick={toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
