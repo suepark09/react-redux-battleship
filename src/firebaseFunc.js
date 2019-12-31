02101a2bfe1718bf026da6b2b1ba47e0e205cb58
Alvin Ng <ngalvin93@gmail.com>
2019-12-30 20:35:00 -0600

    return search result

--- src/firebaseFunc.js
+++ src/firebaseFunc.js
@@ -18,6 +18,7 @@ export function incrementUser () {
 
 export function keyGen (state) {
   // get a key for a new game
+  console.log('SAVING THIS STATE TO DATABASE', state)
   const gameId = firebase.database().ref('/game').push().key
   state.gameId = gameId
   firebase.database().ref('/game').child(gameId).update(state)
@@ -26,9 +27,10 @@ export function keyGen (state) {
 
 export function fetchGameData (gameId) {
   console.log('finding game on database with this id:', gameId)
-  const gameResult = firebase.database().ref('/game').orderByKey().equalTo(gameId)
-  console.log('found this game', gameResult)
-  return
+  const gameResult = firebase.database().ref('/game').orderByKey().equalTo(gameId).on('value', function (snap) {
+    console.log('found this game', snap.val())
+  })
+  return gameResult
 }
 
 // to clear database...
