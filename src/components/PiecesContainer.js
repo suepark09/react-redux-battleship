348be4528679b8914c8b206ade9e990e6d16edff
Sue Park <suepandrice06@gmail.com>
2019-12-30 20:20:08 -0600

    disabled radio btns after setting piece

--- src/components/PiecesContainer.js
+++ src/components/PiecesContainer.js
@@ -16,27 +16,32 @@ class PiecesContainer extends React.Component {
  aircraftCarrier = (e) => {
     console.log('you clicked on aircraft carrier', e.target.value)
     this.props.activate(this.ships[0])
+    document.getElementById("aircraft-carrier").disabled=true;
   }
 
 battleship = (e) => {
     console.log('you clicked on battleship', e.target.value)
     this.props.activate(this.ships[1])
+    document.getElementById("battleship").disabled=true;
   }
 
 submarine = (e) => {
     console.log('you clicked on submarine', e.target.value)
     this.props.activate(this.ships[2])
+    document.getElementById("submarine").disabled=true;
 }
 
 destroyer = (e) => {
     console.log('you clicked on destroyer', e.target.value)
     this.props.activate(this.ships[3])
+    document.getElementById("destroyer").disabled=true;
 }
 
 patrolBoat = (e) => {
     console.log('you clicked on patrol boat', e.target.value)
     this.props.activate(this.ships[4])
     console.log('what is activate', this.props.activate)
+    document.getElementById("patrol-boat").disabled=true;
 }
 
 
@@ -46,16 +51,23 @@ patrolBoat = (e) => {
 // a ship on the board
 
  render() {
+
+    // for orientation, vertical is false and horizontal
+    // is true. by default it is true. 
+    // create function called orientation
+    //if this.props.orientation is true...
+    //call in button using this.orientation
+
     return (
         <div>
           <h3>Your Ships</h3>
           <Form>
             <ul key='inline-radio' className='mb-3 list-unstyled'>
-              <li><Form.Check inline name='ship' type='radio' label='2' value='2' onClick={this.patrolBoat} /></li>
-              <li><Form.Check inline name='ship' type='radio' label='3' value='3' onClick={this.destroyer} /></li>
-              <li><Form.Check inline name='ship' type='radio' label='3' value='3' onClick={this.submarine} /></li>
-              <li><Form.Check inline name='ship' type='radio' label='4' value='4' onClick={this.battleship} /></li>
-              <li><Form.Check inline name='ship' type='radio' label='5' value='5' onClick={this.aircraftCarrier} /></li>
+              <li><Form.Check id="patrol-boat" inline name='ship' type='radio' label='2' value='2' onClick={this.patrolBoat} /></li>
+              <li><Form.Check id="destroyer" inline name='ship' type='radio' label='3' value='3' onClick={this.destroyer} /></li>
+              <li><Form.Check id="submarine" inline name='ship' type='radio' label='3' value='3' onClick={this.submarine} /></li>
+              <li><Form.Check id="battleship" inline name='ship' type='radio' label='4' value='4' onClick={this.battleship} /></li>
+              <li><Form.Check id="aircraft-carrier" inline name='ship' type='radio' label='5' value='5' onClick={this.aircraftCarrier} /></li>
             </ul>
           </Form>
           <h3>Orientation</h3>
