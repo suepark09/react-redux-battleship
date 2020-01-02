166621f0ea10df7c80ddd7bad16a1d1592133b40
jjdelacruz1 <jjdelacruz1@gmail.com>
2020-01-01 18:28:46 -0600

    Update instructions.

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -6,12 +6,12 @@ class Instructions extends React.Component {
     return (
       <ol className="instruction-list">
         <h5 className="instructions-title">Instructions</h5>
-        <li>Share this link with your opponent</li>
         <li>Select your ship to place it</li>
         <li>Choose the orientation: Horizontal or Vertical</li>
         <li>Place all of your ships on the board</li>
-        <li>The game starts when all of the ships are placed on the board</li>
-        <li>Try to destroy all of your opponents ships!</li>
+        <li>Click on the start game button to get a link to share with your opponent</li>
+        <li>The game starts when all of the ships are placed on the board for both players</li>
+        <li>Try to destroy all of your opponents ships before they get yours!</li>
       </ol>
     )
   }
