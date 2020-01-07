d41173c2b4d817c04f203bbddf397c0f44ce96d5
Alvin Ng <ngalvin93@gmail.com>
2020-01-06 21:11:07 -0600

    need to input variable

--- src/components/WinLoseModal.js
+++ src/components/WinLoseModal.js
@@ -3,24 +3,30 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 export default function WinLoseModal() {
 
-    const [modal, setModal] = useState(false)
+    const [modal, setModal] = useState(true)
+    const modalBool = true
 
-    const toggleModal = () => {
+    const toggle = () => {
         console.log('toggle modal')
         setModal(!modal)
     }
 
+    const title = 'TITLE TEXT'
+
+    const body = 'BODY TEXT'
+
     return (
         <React.Fragment>
-            <Modal isOpen={ toggleModal } toggle={ toggleModal } backdrop='true' centered='true'  autoFocus='true'>
-                <ModalHeader className="modal-header" toggle={ toggleModal }>
-                    <h2 className="title-ready">Ready?</h2>
+            <Modal isOpen={ modal } toggle={ toggle } backdrop={ modalBool } centered={ modalBool }  autoFocus={ modalBool }>
+                <ModalHeader className="modal-header" toggle={ toggle }>
+                    <h2 className="title-ready">{ title }</h2>
                 </ModalHeader>
                 <ModalBody className="modal-style">
-                    <p>YOU EITHER WON OR LOST, IDK</p>
+                    <p>{ body }</p>
                 </ModalBody>
                 <ModalFooter className="modal-style">
-                    <Button id='delete-btn' onClick={ toggleModal }>Close</Button>
+                    <Button onClick={ toggle }>Start New Game</Button>
+                    <Button id='delete-btn' onClick={ toggle }>Close</Button>
                 </ModalFooter>
             </Modal>
         </React.Fragment>
